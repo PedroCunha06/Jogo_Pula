638638c77ccec074f46fb0f3d313e4cb062abc05
Pedro Cunha <pdh.alves@outlook.com>
2023-08-25 09:45:54 -0300

    Update 002

--- dino-game/javascript/script.js
+++ dino-game/javascript/script.js
@@ -1,4 +1,26 @@
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+
 const worldElem = document.querySelector('[data-world');
 
 setPixelToWorldScale()
 window.addEventListener('resize', setPixelToWorldScale);
+
+function update(time) {
+    
+}
+
+function setPixelToWorldScale() {
+    let worldToPixelScale;
+    // innerWidht => Retorna a largura da area da janela
+    // innerWHeight => Retorna a altura da area da janela
+    // (window.innerWidth / window.innerHeight) Calcula a proporcao entre a largura e altura da janela
+    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+        worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+    } else {
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
+    }
+
+    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+}
